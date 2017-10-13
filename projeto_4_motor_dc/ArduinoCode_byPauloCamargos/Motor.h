ba714b201a5c10552dbf6dc88b5cdca039796bcd
Italo Fernandes on Ubuntu-Inspiron <italogsfernandes@gmail.com>
2017-10-13 15:55:59 -0300

    Motor.H updated

--- projeto_4_motor_dc/ArduinoCode_byPauloCamargos/Motor.h
+++ projeto_4_motor_dc/ArduinoCode_byPauloCamargos/Motor.h
@@ -2,18 +2,18 @@
 /* DOCUMENTACAO CLASSE MOTOR
  *  Classe desenvolvida por Paulo C. Silva, outubro/2017.
  *  Descri��o: Classe para controle de motores utilizando o modulo l298n.
- *  Atributos: 
- *    _in1 - Pino IN1 conectado do modulo.
- *    _in2 - Pino IN2 conectado do modulo.
- *    _enable - Pino ENABLE do m�dulo.
- *    _sentido  - variavel do tipo enum para armazenamento o sentido de rotaacao 
+ *  Atributos:
+ *    _pinin1 - Pino IN1 conectado do modulo.
+ *    _pinin2 - Pino IN2 conectado do modulo.
+ *    _pinenable - Pino ENABLE do m�dulo.
+ *    _sentido  - variavel do tipo enum para armazenamento o sentido de rotaacao
  *                do motor.
  *    _last_time - Variavel auxiliar para funcao millis();
- *  Metodos: 
+ *  Metodos:
  *    getters() e setters();
- *    void rodaMotor(float f_tensao) - Inicia a rotacao de acordo com a tensao f_tensao. 
+ *    void rodaMotor(float f_tensao) - Inicia a rotacao de acordo com a tensao f_tensao.
  *      - Para f_tensao = 0, motor parado.
- *      - Para f_tensao >= 3.3/2, sentido horario. 
+ *      - Para f_tensao >= 3.3/2, sentido horario.
  *      - Para f_tensao <= 3.3/2, sentido anti-horario.
  *    void dancaMotor(int tempo) - Gira motor com sentido variado e intervalo definido (tempo).
  *    void giraHorario(float tensao) - Gira motor girar em sentido horario.
@@ -21,19 +21,26 @@
  *    void stopMotor(float tensao) - Para o motor.
  *    void desligaMotor() - Desativa (0) os pinos conectados ao madulo.
  */
-enum sentido {ANTI_HORARIO, HORARIO, PARADO};
+//Biblioteca geral do arduino para ter acesso as funções millis(), digitalWrite(), etc...
+#include<Arduino.h>
+
+//Protecao contra incluir 2 vezes a biblioteca
+#ifndef "Motor_H" //Se ainda nao foi incluida
+#define "Motor_H" //Define essa flag para avisar q agr esta sendo incluida
+
+typedef enum sentido_t {ANTI_HORARIO, HORARIO, PARADO};
 
 class Motor {
   private:
-    int _in1;
-    int _in2;
-    int _enable;
-    sentido _sentido;
-    int _last_time; //Variavel auxiliar para a fun��o millis(), ignorar.
+    uint8_t _pinin1;
+    uint8_t _pinin2;
+    uint8_t _pinenable;
+    sentido_t _sentido;
+    uint32_t _last_time; //Variavel auxiliar para a fun��o millis(), ignorar.
   public:
-    Motor(int in1, int in2, int veloc);
-    int getIN1(); void setIN1(int in1);
-    int getIN2(); void setIN2(int in2);
+    Motor(uint8_t in1, uint8_t in2, uint8_t enable);
+    int getIN1(); void setIN1(uint8_t in1);
+    int getIN2(); void setIN2(uint8_t in2);
     int getEnable(); void setEnable(int veloc);
     sentido getSentido(); void setSentido(sentido s);
     void rodaMotor(float f_tensao);
@@ -44,70 +51,72 @@ class Motor {
     void desligaMotor();
 };
 
-Motor::Motor(int in1, int in2, int enable) {
-  this->_in1 = in1;
-  this->_in2 = in2;
-  this->_enable = enable;
+Motor::Motor(uint8_t in1, uint8_t in2, uint8_t enable) {
+  this->_pinin1 = in1;
+  this->_pinin2 = in2;
+  this->_pinenable = enable;
   this->_last_time = 0;
 }
 
 int Motor::getIN1() {
-  return this->_in1;
+  return this->_pinin1;
 }
 
 int Motor::getIN2() {
-  return this->_in2;
+  return this->_pinin2;
 }
 
 int Motor::getEnable() {
-  return this->_enable;
+  return this->_pinenable;
 }
 
-sentido Motor::getSentido() {
+sentido_t Motor::getSentido() {
   return this->_sentido;
 }
 
-void Motor::setIN1(int in1) {
-  this->_in1 = in1;
+void Motor::setIN1(uint8_t in1) {
+  this->_pinin1 = in1;
 }
 
-void Motor::setIN2(int in2) {
-  this->_in2 = in2;
+void Motor::setIN2(uint8_t in2) {
+  this->_pinin2 = in2;
 }
 
-void Motor::setEnable(int enable) {
-  this->_enable = enable;
+void Motor::setEnable(uint8_t enable) {
+  this->_pinenable = enable;
 }
-void Motor::setSentido(sentido s) {
+void Motor::setSentido(sentido_t s) {
   this->_sentido = s;
 }
 
 void Motor::giraHorario(float f_tensao) {
-  int tensao = f_tensao * 100;
-  int cicloPWM = map(tensao, 330 / 2, 330, 0, 255);
+  int tensao = f_tensao * 100; //Cuidado com esses ints declarados aqui
+  //Nao sei se existe Garbage colector nessa linguagem
+  //Talvez você esteja acumulando lixo
+  int cicloPWM = map(tensao, 330 / 2, 330, 0, 255); //Nao entendi mto bem mas deve funcionar
   setSentido(HORARIO);
-  digitalWrite(getIN2(), 0);
-  digitalWrite(getIN1(), 1);
-  analogWrite(getEnable(), cicloPWM);
-  Serial.println("-- SENTIDO HORARIO | TENSAO: " + (String) f_tensao + " V");
+  digitalWrite(_pinin2, 0);
+  digitalWrite(_pinin1, 1);
+  analogWrite(_pinenable, cicloPWM);
+  Serial.println("-- SENTIDO HORARIO | TENSAO: " + new String(f_tensao,2) + " V");
 }
 
 void Motor::giraAntiHorario(float f_tensao) {
   int tensao = f_tensao * 100;
   int cicloPWM = map(tensao, 0, 330 / 2, 0, 255);
   setSentido(ANTI_HORARIO);
-  digitalWrite(getIN1(), 0);
-  digitalWrite(getIN2(), 1);
-  analogWrite(getEnable(), cicloPWM);
-  Serial.println("-- SENTIDO ANTI-HORARIO | TENSAO: " + (String) f_tensao + " V");
+  digitalWrite(_pinin1, 0);
+  digitalWrite(_pinin2, 1);
+  analogWrite(_pinenable, cicloPWM);
+  Serial.println("-- SENTIDO ANTI-HORARIO | TENSAO: " + new String(f_tensao,2) + " V");
 }
 
 void Motor::stopMotor(float f_tensao) { // Freio magn�tico do motor
   setSentido(PARADO);
-  digitalWrite(getIN1(), 1);
-  digitalWrite(getIN2(), 1);
-  analogWrite(getEnable(), 0);
-  Serial.println("-- MOTOR PARADO | TENSAO: " + (String) f_tensao + " V");
+  digitalWrite(_pinin1, 1);
+  digitalWrite(_pinin2, 1);
+  analogWrite(_pinenable, 0);
+  Serial.println("-- MOTOR PARADO | TENSAO: " + new String(f_tensao) + " V");
 }
 
 void Motor::rodaMotor(float f_tensao) {
@@ -118,7 +127,7 @@ void Motor::rodaMotor(float f_tensao) {
     tensao >= 330 / 2 ? giraHorario(tensao) : giraAntiHorario(tensao);
 }
 
-void Motor::dancaMotor(float f_tensao, int tempo) {
+void Motor::dancaMotor(float f_tensao, int tempo) { //Haha
   int tensao = f_tensao * 100;
   if (millis() - _last_time > tempo) {
     _last_time = millis();
@@ -128,9 +137,10 @@ void Motor::dancaMotor(float f_tensao, int tempo) {
 
 void Motor::desligaMotor(){
   setSentido(PARADO);
-  digitalWrite(getIN1(), 0);
-  digitalWrite(getIN2(), 0);
-  analogWrite(getEnable(), 0);
+  digitalWrite(_pinin1, 0);
+  digitalWrite(_pinin2, 0);
+  analogWrite(_enable, 0);
   Serial.println("-- MOTOR DESLIGADO ---------- ");
 }
+#endif /*Motor_h*/
 // --------------------------- FIM DA CLASSE Motor.h -----------------
--- projeto_4_motor_dc/ArduinoCode_byPauloCamargos/MotorLab04.ino
+++ projeto_4_motor_dc/ArduinoCode_byPauloCamargos/MotorLab04.ino
@@ -1,24 +1,38 @@
-#include "Motor.h" // Import classe Motor.h, que deve estar na mesma pasta deste arquivo .ino
+////////////////
+//Bibliotecas //
+////////////////
+#include "Motor.h" // Import a library Motor.h, que deve estar na mesma pasta deste arquivo .ino
 
-Motor *motor;     //
-
+////////////
+//Defines //
+////////////
 #define DANCE_BUTTON 8    // Ignorar. Apenas funcao extra
 #define LD_BUTTON 7       // Botão liga-desliga do sistema
 #define FR_BUTTON 6       // Botão frei magnético do sistema
 #define POT_V A0          // Pino de leitura da tensão no potenciometro
 
+////////////////////////////////
+//Variaveis e objetos globais //
+////////////////////////////////
+
+// Cria um objeto 'motor' da classe 'Motor' e inicializacao dos pinos Motor(IN1, IN2, ENABLE);
+Motor motor(13, 12, 10); // Por que era um ponteiro?
 //Variaveis para debouncing do LD_BUTTON
-bool chButtonState, lastChButtonState = 0;
+bool chButtonState, lastChButtonState = 0; //Ae sim
 unsigned long lastDeboucingTime = 0, debounceDelay = 50;
 bool estadoSistema = 1;
 
+////////////////
+//Main Sketch //
+////////////////
+/**
+ * Iniciliza os pinos como saidas e entradas e tambem a comunicação serial
+ */
 void setup() {
   /* Informações da classe motor*/
-  // Cria um objeto 'motor' da classe 'Motor' e inicializacao dos pinos Motor(IN1, IN2, ENABLE);
-  motor = new Motor(13, 12, 10);
-  pinMode(motor->getIN1(), OUTPUT);     // Configuracao do pino IN1 como OUTPUT
-  pinMode(motor->getIN2(), OUTPUT);     // Configuracao do pino IN2 como OUTPUT
-  pinMode(motor->getEnable(), OUTPUT);  // Configuracao do pino ENABLE como OUTPUT
+  pinMode(motor.getIN1(), OUTPUT);     // Configuracao do pino IN1 como OUTPUT
+  pinMode(motor.getIN2(), OUTPUT);     // Configuracao do pino IN2 como OUTPUT
+  pinMode(motor.getEnable(), OUTPUT);  // Configuracao do pino ENABLE como OUTPUT
 
   //Variáveis internas do programa (botoes)
   pinMode(DANCE_BUTTON, INPUT_PULLUP);  // Configuracao do pino DANCE_BUTTON como INPUT_PULLUP
@@ -29,20 +43,37 @@ void setup() {
   Serial.println(" -------- PROGRAMA EM FUNCIONAMENTO -------- ");
 }
 
+/**
+ * Se repete a cada ciclio, cuidando de executar a tarefa desejada
+ * - Leitura da tensao em A0
+ * - Controle do motor de acordo com a Leitura em A0
+ * - Controle do motor de acordo com os botões
+ */
 void loop() {
   float f_tensao = calculaTensao();       // Chamada da funcao para calculo da tensao no potenc.
   //Ativa o freio mag. se o motor estiver funcionando e a chave FR_BUTTON pressionada
   if (!digitalRead(FR_BUTTON) && ligaDesliga()) {
-    motor->stopMotor(f_tensao);          // Metodo do objeto 'motor' para freio magnetico
+    motor.stopMotor(f_tensao);          // Metodo do objeto 'motor' para freio magnetico
   } else      // Operador ternario
-    // Se o sistema estiver ligado, faz o motor funcionar. Caso contrario, desliga motor.
-    ligaDesliga() ? motor->rodaMotor(f_tensao) : motor->desligaMotor();
+  // Se o sistema estiver ligado, faz o motor funcionar. Caso contrario, desliga motor.
+  ligaDesliga() ? motor.rodaMotor(f_tensao) : motor.desligaMotor();
 }
 
+//////////////////////
+//Metodos Auxilires //
+/////////////////////
+
+/**
+ * [calculaTensao description]
+ * @return [description]
+ */
 float calculaTensao() {  // funcao para calculo da tensao no potenc.
   return map(analogRead(POT_V), 0, 1023, 0, 330) / 100.0;
 }
-
+/**
+ * [ligaDesliga description]
+ * @return [description]
+ */
 bool ligaDesliga() { // Debounce do botao liga desliga. Retorna 1 para ligado, 0 para desligado.
   // Ver referencia em https://www.arduino.cc/en/Tutorial/Debounce para entendimento da funcao
   int chReading = !digitalRead(LD_BUTTON);
@@ -61,4 +92,3 @@ bool ligaDesliga() { // Debounce do botao liga desliga. Retorna 1 para ligado, 0
   lastChButtonState = chReading;
   return estadoSistema;
 }
-
